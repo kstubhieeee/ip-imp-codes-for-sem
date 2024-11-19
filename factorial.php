2672862909c5985d277053ccc72ca14533d89a7c
kstubhie <banekaustubh27@gmail.com>
2024-11-19 23:37:32 +0530

    factorial program

---
+++ factorial.php
@@ -0,0 +1,15 @@
+<?php
+function factorial($number) {
+    if ($number < 0) {
+        return "Factorial is not defined for negative numbers.";
+    }
+    $result = 1;
+    for ($i = 1; $i <= $number; $i++) {
+        $result *= $i;
+    }
+    return $result;
+}
+
+$number = 5;
+echo "The factorial of $number is " . factorial($number);
+?>
